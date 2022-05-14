a66930ad0f503ad457e7c7933ab64e675e03237a
anim-osity <55509921+anim-osity@users.noreply.github.com>
2022-05-14 15:18:44 -0500

    Add files via upload

---
+++ miscellaneous Visual studio projects/Customer Solution/Customer Project/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FirstName = New System.Windows.Forms.TextBox()
+        Me.LastName = New System.Windows.Forms.TextBox()
+        Me.Address = New System.Windows.Forms.TextBox()
+        Me.City = New System.Windows.Forms.TextBox()
+        Me.ZIP = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&First:"
+        '
+        'FirstName
+        '
+        Me.FirstName.Location = New System.Drawing.Point(57, 46)
+        Me.FirstName.Name = "FirstName"
+        Me.FirstName.Size = New System.Drawing.Size(121, 20)
+        Me.FirstName.TabIndex = 5
+        '
+        'LastName
+        '
+        Me.LastName.Location = New System.Drawing.Point(265, 46)
+        Me.LastName.Name = "LastName"
+        Me.LastName.Size = New System.Drawing.Size(187, 20)
+        Me.LastName.TabIndex = 6
+        '
+        'Address
+        '
+        Me.Address.Location = New System.Drawing.Point(79, 112)
+        Me.Address.Multiline = True
+        Me.Address.Name = "Address"
+        Me.Address.Size = New System.Drawing.Size(369, 20)
+        Me.Address.TabIndex = 7
+        '
+        'City
+        '
+        Me.City.Location = New System.Drawing.Point(54, 183)
+        Me.City.Name = "City"
+        Me.City.Size = New System.Drawing.Size(245, 20)
+        Me.City.TabIndex = 8
+        '
+        'ZIP
+        '
+        Me.ZIP.Location = New System.Drawing.Point(375, 184)
+        Me.ZIP.Name = "ZIP"
+        Me.ZIP.Size = New System.Drawing.Size(77, 20)
+        Me.ZIP.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(223, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "&Last:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(15, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "&Address:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(15, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 16)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "&City:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(338, 185)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "&ZIP:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(377, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "&Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(15, 239)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(96, 13)
+        Me.lblMessage.TabIndex = 15
+        Me.lblMessage.Text = "Information Saved."
+        Me.lblMessage.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(475, 261)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ZIP)
+        Me.Controls.Add(Me.City)
+        Me.Controls.Add(Me.Address)
+        Me.Controls.Add(Me.LastName)
+        Me.Controls.Add(Me.FirstName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Customer Infomration"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents FirstName As TextBox
+    Friend WithEvents LastName As TextBox
+    Friend WithEvents Address As TextBox
+    Friend WithEvents City As TextBox
+    Friend WithEvents ZIP As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblMessage As Label
+End Class
---
+++ miscellaneous Visual studio projects/Customer Solution/Customer Project/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Dim FirstNamee As String = ""
+    Dim LastNamee As String = ""
+    Dim Addresss As String = ""
+    Dim Cityy As String = ""
+    Dim ZIPP As String = ""
+
+    Dim sr As New StreamWriter("customers.txt") ' crates a text file under the name of 'customers'
+
+    ' Each text box's Enter even procedure
+    Private Sub FirstName_Enter(sender As Object, e As EventArgs) Handles FirstName.Enter
+        FirstNamee = FirstName.Text
+    End Sub
+    Private Sub LastName_Enter(sender As Object, e As EventArgs) Handles LastName.Enter
+        LastNamee = LastName.Text
+    End Sub
+    Private Sub Address_Enter(sender As Object, e As EventArgs) Handles Address.Enter
+        Addresss = Address.Text
+    End Sub
+    Private Sub City_Enter(sender As Object, e As EventArgs) Handles City.Enter
+        Cityy = City.Text
+    End Sub
+    Private Sub ZIP_Enter(sender As Object, e As EventArgs) Handles ZIP.Enter
+        ZIPP = ZIP.Text
+    End Sub
+
+    ' Issue is that enter is not working to get the values for each vairable
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Together As String = FirstNamee & " " & LastNamee
+        Dim Place As String = Addresss & ", " & Cityy & ", " & ZIPP
+        Dim Spaces As Integer = 0
+
+
+        ' Checks if at least one of the textboxes are empty
+        If (FirstName.Text = String.Empty Or LastName.Text = String.Empty Or Address.Text = String.Empty Or City.Text = String.Empty Or ZIP.Text = String.Empty) Then
+            MessageBox.Show("One of the textboxes are empty, please double check that all boxes are filled out", "Incomplete information")
+        Else
+
+            If (File.Exists("customers.txt")) Then
+
+                sr.WriteLine(Together)
+                sr.WriteLine(Place)
+                sr.Close()
+                lblMessage.Visible = True
+                Spaces = 1
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub frmMain_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+
+        If (MessageBox.Show("Are you sure you want to exit?", "", MessageBoxButtons.YesNo) = DialogResult.No) Then ' If the user doesn't want to close, then the form stays open 
+            e.Cancel = True
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_TextChanged(sender As Object, e As EventArgs) Handles FirstName.TextChanged, LastName.TextChanged, Address.TextChanged, City.TextChanged, ZIP.TextChanged
+        lblMessage.Visible = False ' If any of the textboxes' text are changed then the 'information saved' text disappears 
+    End Sub
+
+
+End Class
