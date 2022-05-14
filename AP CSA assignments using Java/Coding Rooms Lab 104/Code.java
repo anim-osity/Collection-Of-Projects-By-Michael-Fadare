fd4565b31510bd1769a597a242350722a41991ad
anim-osity <55509921+anim-osity@users.noreply.github.com>
2022-05-14 15:06:23 -0500

    Add files via upload

---
+++ AP CSA assignments using Java/Coding Rooms Lab 104/Code.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Main
+{
+	public static ArrayList<String> test()
+	{
+		ArrayList<String> names = new ArrayList<String>();
+		//write code below
+		names.add("Sam");
+		names.add("Ted");
+		names.add("Jack");
+		
+		//leave below code alone
+		return names;
+	}
+}
