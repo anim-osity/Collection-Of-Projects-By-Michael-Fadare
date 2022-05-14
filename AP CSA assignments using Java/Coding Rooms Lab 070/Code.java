5057a90d473b781373beccfe527795bacae2cc21
anim-osity <55509921+anim-osity@users.noreply.github.com>
2022-05-14 15:25:34 -0500

    Add files via upload

---
+++ AP CSA assignments using Java/Coding Rooms Lab 070/Code.java
@@ -0,0 +1,16 @@
+class Main
+{
+	public static void main(String[] args)
+	{
+		int[] ar = new int[]{10,20,30,40,50};
+
+         for (int i = 0; i < ar.length; i++) {
+  
+            // accessing each element of array
+            int x = ar[i];
+            System.out.print(x + " ");
+            }
+
+		
+	}
+}
